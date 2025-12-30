2a77752bb9d08b2e74090276a512f56aa1e7939f
rejo-sudo <rejobravo@gmail.com>
2025-12-30 19:27:08 +0530

    task1-task2 python code
    added task1-task2

---
+++ task1.py
@@ -0,0 +1,7 @@
+fn = float(input("Enter the first number: "))
+sn = float(input("Enter the second number: "))
+
+print("Addition: ", round(fn + sn))
+print("Subtraction: ", int(fn - sn))
+print("Multiplication: ", round(fn * sn))
+print("Division: ", fn / sn)
---
+++ task2.py
@@ -0,0 +1,4 @@
+f_name = input("Enter first name: ")
+l_name = input("Enter last name: ")
+
+print("Hello,", f_name, " ", l_name,"! Welcome to Python Program.", sep="")
